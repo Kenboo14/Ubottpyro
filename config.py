2baebfce0b002cdfb353fd2e0290549cc2d33ff5
Kenboox <116920242+Kenboo14@users.noreply.github.com>
2023-09-15 02:12:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -14,20 +14,20 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 
-API_HASH = getenv("API_HASH")
-API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "af53cce8dc3b6fb2e4ae0c17e9d646df")
+API_ID = int(getenv("API_ID", "25989336"))
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
-    BLACKLIST_CHAT = [-1001473548283]
+    BLACKLIST_CHAT = [-1001675452200]
 BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
-BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or -1001711930627)
 BOT_VER = "0.2.0@main"
 BRANCH = getenv("BRANCH", "main")
-CHANNEL = getenv("CHANNEL", "Lunatic0de")
+CHANNEL = getenv("CHANNEL", "rexc0de")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
-DB_URL = getenv("DATABASE_URL", "")
+DB_URL = getenv("DATABASE_URL", "postgres://nhtwommz:QNNAdV0MufL8fHWZo4IBKBrwlijCWSjX@ruby.db.elephantsql.com/nhtwommz")
 GIT_TOKEN = getenv("GIT_TOKEN", "")
-GROUP = getenv("GROUP", "SharingUserbot")
+GROUP = getenv("GROUP", "TongmetLovers")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
